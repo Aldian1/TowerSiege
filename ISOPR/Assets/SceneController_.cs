97853cc51ae327e7a6caae5b816e1779d54c1e89
Aldian1 <Aldian@hotmail.co.uk>
2016-07-23 00:12:53 +0100

    Day1

---
+++ ISOPR/Assets/SceneController_.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneController_ : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        if(GUILayout.Button("Reload Scene"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        }
+
+    }
+
+}
