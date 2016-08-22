2fc89b61dc429fdd4b313ec54750f1716dc227e3
Aldian1 <Aldian@hotmail.co.uk>
2016-08-23 00:06:49 +0100

    1.0.3.4
    So the building system has been rewritten from scratch. Using UI buttons just made the codebase messy and unnecisarily long. Ive gone for good old mouse enter mouse exit systems at the moment.
    
    Lighting has been reworked. Switched from anti aliasing to HDR rendering. Played with some emmission tests too.
    
    Need to finish the building system and get the team to try out.
    
    Also need to fix the turret orientations.
    
    Rew

---
+++ ISOPR/Assets/Scripts/GridDetector.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridDetector : MonoBehaviour {
+
+    public Material normal, highlighted;
+
+    public bool mousein,locked;
+
+    private SceneController_ SceneController;
+	// Use this for initialization
+	void Start () {
+        SceneController = Camera.main.GetComponent<SceneController_>();
+
+            normal = GetComponent<Renderer>().material;
+            highlighted = Resources.Load("GridHighlight") as Material;
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	if(Input.GetKeyDown(KeyCode.Mouse0) && mousein == true && locked == false && SceneController.CurrentlySelected != null)
+        {
+            
+            Vector3 pos = new Vector3(this.transform.position.x, this.transform.position.y + 10, this.transform.position.z);
+            GameObject go = Instantiate(SceneController.CurrentlySelected, pos, Quaternion.identity) as GameObject;
+            StartCoroutine("TweenCube", go);
+            go.transform.parent = this.gameObject.transform;
+            locked = true;
+        }
+	}
+
+
+    void OnMouseEnter()
+    {
+        if (!locked)
+        {
+            this.GetComponent<Renderer>().material = highlighted;
+            mousein = true;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        this.GetComponent<Renderer>().material = normal;
+        mousein = false;
+    }
+
+
+    //tweener = the gameobject 
+
+    public IEnumerator TweenCube(GameObject tweener)
+    {
+
+
+        Vector3 brickpos = new Vector3(this.transform.position.x, this.transform.position.y + 1, this.transform.position.z);
+
+        while (Vector3.Distance(tweener.transform.position, this.transform.position) > 1)
+        {
+            tweener.transform.Translate(Vector3.down * Time.deltaTime * 3);
+            yield return new WaitForSeconds(.03F);
+
+        }
+        this.enabled = false;
+
+    }
+}
---
+++ ISOPR/Assets/Scripts/SceneController_.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneController_ : MonoBehaviour {
+	float deltaTime = 0.0f;
+	public float gold;
+	public GameObject[] Enemies;
+
+    //the current object to be built
+    public GameObject CurrentlySelected;
+
+    //reference to all the buildable objects
+    public GameObject[] Buildables;
+
+    //the main parent of all the island objects
+    public GameObject IslandParent;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+	}
+
+    void OnGUI()
+    {
+
+
+
+
+
+		//GUILayout.
+		GUILayout.Label ("Debug Menu/Stats");
+		GUILayout.Label ("Current Gold: " + gold);
+
+        if(GUILayout.Button("Reload Scene"))
+        {
+			
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        }
+
+
+
+
+
+		//fps calculations
+		int w = Screen.width, h = Screen.height;
+
+		GUIStyle style = new GUIStyle();
+
+		Rect rect = new Rect(w/4, 0, w, h * 2 / 100);
+		style.alignment = TextAnchor.UpperLeft;
+		style.fontSize = h * 2 / 100;
+		style.normal.textColor = Color.yellow;
+		float msec = deltaTime * 1000.0f;
+		float fps = 1.0f / deltaTime;
+		string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+		GUI.Label(rect, text, style);
+
+    }
+
+    //selects the item based on what number the button sends. 
+    public void SelectItem(int IndexNumber)
+    {
+        CurrentlySelected = Buildables[IndexNumber];
+    }
+    //cancel out of building so if we click on something we wont build anything
+    public void Cancel()
+    {
+        CurrentlySelected = null;
+    }
+    //rotates the island so we can see the other side
+    public void RotateIsland()
+    {
+     if(IslandParent.transform.rotation.y < 180)
+        {
+            IslandParent.transform.Rotate(0,180,0);
+        }
+        else
+        {
+            IslandParent.transform.Rotate(0, 0, 0);
+        }
+    }
+}
